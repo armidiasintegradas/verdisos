f77c6f8fa0390dd0997664a26294d67969d9381c
armidiasintegradas <armidiasintegradas@gmail.com>
2026-09-21 09:22:41 -0300

    test(team): correct pgTAP plan count

--- supabase/tests/database/0018_cooperative_user_management.test.sql
+++ supabase/tests/database/0018_cooperative_user_management.test.sql
@@ -3,7 +3,7 @@ begin;
 create extension if not exists pgtap with schema extensions;
 set local search_path = public, extensions;
 
-select plan(12);
+select plan(13);
 
 select has_function(
   'public'::name,
